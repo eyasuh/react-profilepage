5c3cb38212982a1e33181277de3bf9bfb236aa37
eyasuh <eyha12@gmail.com>
2022-06-04 18:40:34 -0400

    introduction components and css added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header.js';
 import Footer from "./Components/Footer/Footer";
+import Intro from "./Components/Introduction/Intro";
 
 function App() {
     return (
         <div className="App">
             <Header className="App-header"/>
-            {/*<Introduction />*/}
+            <Intro />
             {/*<Projects />*/}
             {/*<Achievements />*/}
             {/*<Contacts />*/}
---
+++ src/Components/Introduction/Intro.js
@@ -0,0 +1,20 @@
+import './Intro.css';
+
+function Intro() {
+    return (
+        <>
+        <main>
+            <section>
+                <article>
+                    <h4>WELLCOME TO MY PAGE, MY NAME IS</h4>
+                    <h1>Eyasu Haile</h1>
+                    <p>I am a full-stack developer</p>
+                    <a className='resume-link' href="#">CHECK OUT MY RESUME</a>
+                </article>
+            </section>
+        </main>
+        </>
+    )
+}
+
+export default Intro;
