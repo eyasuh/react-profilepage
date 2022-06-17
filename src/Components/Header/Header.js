00e83d8c3426e1fdc587e6e3083473bb6f373d00
eyasuh <eyha12@gmail.com>
2022-06-17 13:44:45 -0400

    svg log and resume added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,19 @@
 import './Header.css';
-import logo from './logo.png';
+import logo from './logo.svg';
+import resume from './Resume-2.pdf'; 
 
 function Header() {
     return (
         <header className='app-header'>
             <a href='#home'>
-                <img className='logo' src={logo} alt='svg logo' />
+                <img className='logo' src={logo} alt='svg logo'/>
             </a>
             <nav role="navigation">
                 <ul className="navbar">
-                    <li><a href="#">Home</a></li>
                     <li><a href="#">About Me</a></li>
-                    <li><a href="https://github.com/eyasuh" target="blanck">GitHub<i
+                    <li><a href="#">Projects</a></li>
+                    <li><a href={resume}>Resume</a></li>
+                    <li><a href="https://github.com/eyasuh" target="blanck">GitHub <i
                         className="fa-brands fa-github-square"></i>
                     </a>
                     </li>
