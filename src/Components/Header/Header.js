797be246becc92c7867aba4158dac385aca24c02
eyasuh <eyha12@gmail.com>
2022-06-27 22:57:22 -0400

    Updated styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,19 @@
 import './Header.css';
 import logo from './logo.svg';
-import resume from '../utilities/Resume-2.pdf'; 
+import resume from '../utilities/Resume-2.pdf';
 import ContainedButton from '../ContainedButton/ContainedButton';
+import { MenuOutlined } from '@ant-design/icons';
+import "antd/dist/antd.css";
+import { useState } from 'react'
 
 function Header() {
-
     return (
         <header className='app-header'>
             <a href='#home'>
-                <img className='logo' src={logo} alt='svg logo'/>
+                <img className='logo' src={logo} alt='svg logo' />
             </a>
-            <nav role="navigation">
-                <ul className="navbar">
+            <nav role="navigation" >
+                <ul className="navbar" id='navbar-list'>
                     <li><a href="#aboutme-article">About Me</a></li>
                     <li><a href="#project" >Projects</a></li>
                     <li><a href={resume}>Resume</a></li>
@@ -19,8 +21,8 @@ function Header() {
                         className="fa-brands fa-github-square"></i>
                     </a>
                     </li>
-                    <ContainedButton href='#footer' buttonName='Contact Me'/> 
                 </ul>
+                <MenuOutlined className='humberger' id='humberger-menu' />
             </nav>
             {/* <hr /> */}
         </header>
